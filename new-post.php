9a566fdc1fbff759c45c30d8cf843d74e02a6396
haritdheer <haritdheer@gmail.com>
2022-08-08 04:09:59 +0530

    newpost signsignup and index category db connected

--- new-post.php
+++ new-post.php
@@ -20,21 +20,45 @@
   </head>
 
   <body class="text-center">
-    <form class="form-signin">
+    <form class="form-signin" action="new-post.php" method="POST">
       <img class="mb-4" src="https://getbootstrap.com/docs/4.0/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">
       <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
-      <label for="inputEmail" class="sr-only">Email address</label>
-      <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+      <label for="inputEmail"  class="sr-only">Email address</label>
+      <input type="email" id="inputEmail" name="email" class="form-control" placeholder="Email address" required autofocus>
       <label for="inputPassword" class="sr-only">Password</label>
-      <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+      <input type="password" id="inputPassword" name="password" class="form-control" placeholder="Password" required>
       <div class="checkbox mb-3">
-        <label>
+        <!-- <label>
           <input type="checkbox" value="remember-me"> Remember me
-        </label>
+        </label> -->
       </div>
-      <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-      <a href="signup.php">Create a account</a>
+      <input class="btn btn-lg btn-primary btn-block" name ="submit" value="Sign In" type="submit">
+      <!-- <a href="signup.php">Create a account</a> -->
       <p class="mt-5 mb-3 text-muted">&copy; 2022</p>
     </form>
   </body>
 </html>
+
+<?php
+include('connection/db.php');
+if(isset($_POST['submit'])){
+   $email=$_POST['email'];
+  $pass=$_POST['password'];
+
+ $query=mysqli_query($conn,"select * from admin_login where admin_email='$email' and admin_pass='$pass' and admin_type='2' ");
+
+if($query){
+
+if(mysqli_num_rows($query)>0){
+
+$_SESSION['email']=$email;
+header('location:admin/admin_dashboard.php');
+
+}else{
+  echo "<script>alert('Email or password incorrect!!Please try again')</script>";
+}
+}
+
+}
+
+?>
